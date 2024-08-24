8b729468776e0a18bc947fbc9e735b90ca4cbb50
Omar Taraki <otaraki@student.1337.ma>
2024-08-24 22:55:40 +0000

    first commit

---
+++ core/demoApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader 
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello world")
+
+def index(request): 
+    path = request.path 
+    method = request.method 
+    content=''' 
+<center><h2>Testing Django Request Response Objects</h2> 
+<p>Request path : " {}</p> 
+<p>Request Method :{}</p></center> 
+'''.format(path, method) 
+    return HttpResponse(content) 
+
+def pathview(request, name, id): 
+    return HttpResponse("Name:{} UserID:{}".format(name, id)) 
+
+# def index(request): 
+#     template = loader.get_template('template/index.html') 
+#     context={}  
+#     return HttpResponse(template.render(context, request)) 
