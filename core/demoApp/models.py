fd3143076761465d87e2ffae22cd3d4d9d6efeaf
Omar Taraki <otaraki@student.1337.ma>
2024-08-25 23:42:42 +0000

    migrate tow models ..

--- core/demoApp/models.py
+++ core/demoApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class college(models.Model):
+    collegeId = models.IntegerField(primary_key = True)
+    name = models.CharField(max_length = 50)
+    strength = models.IntegerField()
+    website = models.URLField()
+
+class Principale(models.Model):
+    CollegId = models.OneToOneField(
+        college,
+        on_delete = models.CASCADE
+    )
+    Qualification = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
